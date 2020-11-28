a6739df9ad86af8b192c9c10a821060d3ee32e54
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-11-28 20:25:01 +0100

    added description

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from utils import *
 
+tags_metadata=[
+    {
+        'name':'similarity',
+        'description':'Finds the similarity between 2 sentences using their word vectors.'
+    },
+    {
+        'name':'tokenize',
+        'description':'Takes in word, sentences e.t.c and return lexical infromation about each of words. e.g Nouns, Abstract Nouns, Co-ordinating conjunction.'
+    },
+    {
+        'name':'synonyms',
+        'description':'Takes in a word or a group of words separated by commas and return a list of english language synonyms for the words.'
+    },
+    {
+        'name':'antonyms',
+        'description':'Takes in a word or a group of words separated by commas and return a list of english language antonyms for the words.'
+    },
+    {
+        'name':'tospeech',
+        'description':'Takes in a string and returns an audio file of the word.'
+    }
+]
+
 class SimilarityIn(BaseModel):
     text_1 : str
     text_2 : str
@@ -19,23 +42,25 @@ class AntonymsIn(BaseModel):
 class TextToSpeech(BaseModel):
     text : str
     language : Optional[str] = 'en'
-app = FastAPI()
+app = FastAPI(title='Tageit',
+              description='This is a hobby project for people interesed in using NLP. Email tomibami2020@gmail.com for new functionality you want to be added.',
+              openapi_tags=tags_metadata)
 
 @app.get('/')
 def home():
-    return "Welcome here"
+    return 'Welcome here'
 
-@app.post('/similarity' , response_model=SimilarityOut)
+@app.post('/similarity' , response_model=SimilarityOut,tags=['similarity'])
 def similarity(text : SimilarityIn):
     score = similarity_(text.text_1, text.text_2)
     return {'score':score}
 
-@app.post('/tokenize', response_model=dict)
+@app.post('/tokenize', response_model=dict,tags=['tokenize'])
 def tokenize(text : TokenizeIn):
     tokens = tokenize_(text.text)
     return tokens
 
-@app.post('/synonyms', response_model=dict)
+@app.post('/synonyms', response_model=dict, tags=['synonyms'])
 def synonyms(text : SynonymIn ):
     words = text.text.replace(' ','').split(',')
     response = {}
@@ -45,7 +70,7 @@ def synonyms(text : SynonymIn ):
         
     return response
 
-@app.post('/antonyms', response_model=dict)
+@app.post('/antonyms', response_model=dict, tags=['antonyms'])
 def antonyms(text : AntonymsIn ):
     words = text.text.replace(' ','').split(',')
     response = {}
@@ -54,7 +79,7 @@ def antonyms(text : AntonymsIn ):
         response[i]=syns       
     return response
 
-@app.post('/tospeech')
+@app.post('/tospeech' ,tags=['tospeech'])
 async def  text_to_speech(text : TextToSpeech ):
     language = text.language
     if len(language)>2:
